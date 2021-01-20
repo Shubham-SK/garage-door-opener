3c1cf2c16b227d841860a6fb463504d57d57833b
Shubham Kumar <kumar.shubham5504@gmail.com>
2021-01-19 16:25:53 -0800

    Touch Screen GUI & TFT Updates

--- oren.ino
+++ oren.ino
@@ -51,29 +51,107 @@ void setup() {
     pinMode(relay, OUTPUT);
     digitalWrite(relay, LOW);
     
-    // screen initialization stuff
+    // screen display initialization stuff
     Serial.begin(9600);
     tft.begin();
     tft.setRotation(90);
     tft.fillScreen(ILI9341_BLACK);
-  
-    tft.setCursor(0, 0);
-    tft.setTextColor(ILI9341_RED);  
-    tft.setTextSize(3);
-    tft.println("Control");
-    tft.println("System");
     
-    tft.setCursor(0, 60);
-    tft.setTextColor(ILI9341_GREEN);  
-    tft.setTextSize(1);
-    tft.println("All Resources Active");
+    // screen touch initialization stuff
+    pinMode(xpos, OUTPUT);
+    pinMode(yneg, OUTPUT);
+    digitalWrite(xpos, HIGH);
+    digitalWrite(yneg, HIGH);
+    pinMode(xneg, INPUT);
+    pinMode(ypos, INPUT);
+    
+    // time stuff
+    Time.zone(-8);
 }
 
 //////////
 // LOOP //
 //////////
 void loop() {
+    // formatting time
+    String timeString = Time.format(Time.now(), "%I:%M %p");
+    String dateString = Time.format(Time.now(), "%A, %b %d");
     
+    // init
+    tft.setCursor(0, 0);
+    
+    // writing time
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+    tft.setTextSize(4);
+    tft.println(timeString);
+    
+    // writing date
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+    tft.setTextSize(2);
+    tft.println(dateString);
+    
+    // spacing
+    tft.setTextSize(1);
+    tft.println();
+    tft.println();
+    
+    // wifi status
+    WiFiSignal sig = WiFi.RSSI();
+    int strength = sig.getStrength();
+    int quality = sig.getQuality();
+    
+    tft.setTextSize(1);
+    if(WiFi.ready()){
+        tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);  
+        tft.println("Resources Active");
+    } else {
+        tft.setTextColor(ILI9341_RED, ILI9341_BLACK);  
+        tft.println("Resources Inactive");
+    }
+    tft.println("WiFi strength: " + String(strength) + "%");
+    tft.println("WiFi quality: " + String(quality) +  "%");
+    
+    // spacing
+    tft.setTextSize(1);
+    tft.println();
+    
+    // weather data
+    tft.setTextSize(1);
+    tft.setTextColor(ILI9341_BLUE, ILI9341_BLACK);  
+    tft.println("Cloudy");
+    tft.println("Temperature: 70 DEG");
+    tft.println("Precipitation: 0%");
+    tft.println("Wind Speed: 25 MPH");
+    
+    // spacing
+    tft.setTextSize(1);
+    tft.println();
+    tft.println();
+    
+    // status
+    tft.setTextSize(2);
+    String stateCopy = state;
+    stateCopy.toUpperCase();
+    if(stateCopy == "OPEN"){
+        tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);
+    } else {
+        tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
+    }
+    tft.println("STATUS:"+stateCopy);
+    
+    // spacing
+    tft.setTextSize(1);
+    tft.println();
+    
+    // prompt
+    tft.setTextSize(1);
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+    tft.println("Push Below To Toggle:");
+    
+    // spacing
+    tft.setTextSize(1);
+    tft.println("x: "+String(analogRead(xneg)));
+    tft.println("y: "+String(analogRead(ypos)));
 }
 
 
